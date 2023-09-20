2a47785e3ee687e0abadac900f8d22828e4aec85
jihyung2 <wlgud1116@naver.com>
2023-09-20 17:33:27 +0900

    2번 끝물

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route('/submit', methods=['POST'])
 def submit():
     # 사용자가 제출한 코드 받기
@@ -34,6 +33,29 @@ def submit():
     except subprocess.TimeoutExpired:
         return jsonify({"error": "Time limit exceeded"})
 
+@app.route('/addProblemRunCode', methods=['POST'])
+def addProblemRunCode():
+    # 사용자가 등록한 코드 받기
+    code = request.json.get('code')
+
+    # 코드를 .py 파일로 저장하기
+    file_path = 'UserAnswer/problem_code.py'
+    with open(file_path, 'w') as f:
+        f.write(code)
+
+    # 테스트 케이스 실행하고 결과 확인하기
+    try:
+        output = subprocess.check_output(['python3', file_path], timeout=5)
+        output = output.decode('utf-8')
+
+        # 파일 삭제하기
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+        return output
+
+    except subprocess.TimeoutExpired:
+        return jsonify({"error": "Time limit exceeded"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8090)
