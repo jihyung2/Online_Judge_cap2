f4203cd4ca9cbec52af0a03a51dda710f0103471
jihyung2 <wlgud1116@naver.com>
2023-09-21 17:02:45 +0900

    기본 틀 완성

--- app.py
+++ app.py
@@ -12,20 +12,30 @@ app = Flask(__name__)
 @app.route('/submit', methods=['POST'])
 def submit():
     # 사용자가 제출한 Base64 인코딩된 코드와 예상 출력값 받기
-    encoded_code = request.json.get('code')
+    code = request.json.get('code')
+    code  = eval('f' + f'"{code}"')
+
     expected_output_str = request.json.get('output')
+    filename = request.json.get('name')
+    print(code,expected_output_str)
 
-    # Base64 인코딩된 코드를 디코딩하기
-    code = base64.b64decode(encoded_code).decode('utf-8')
+    current_directory = os.getcwd()
 
-    # 디코딩된 코드를 .py 파일로 저장하기
-    with open('UserAnswer/user_code.py', 'w') as f:
+    # 상대 경로 설정
+    relative_path = "UserAnswer"  # 현재 디렉토리 기준으로 UserAnswer 디렉토리
+    filename = filename+"_usercode.py"
+    file_path = os.path.join(current_directory, relative_path, filename)
+
+    # 파일 저장
+    with open(file_path, 'w') as f:
         f.write(code)
 
     # 테스트 케이스 실행하고 결과 확인하기
     try:
-        output = subprocess.check_output(['python3', 'UserAnswer/user_code.py'], timeout=5)
+        output = subprocess.check_output(['python3', file_path], timeout=5)
         output = output.decode('utf-8')
+        print(output)
+        print(expected_output_str)
 
         if output == expected_output_str:
             return jsonify({"result": "Correct", "output": output})
